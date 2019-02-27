7bc8382bff4a0eebbb31f207d69c9c2bd3fe8363
Gabriel Hans Gonzalez <ghans@intecfra.com>
2019-02-27 17:32:53 -0600

    Configuración del service provider
    Se cambia la configuración del Service Provider usando el facade config en variable

--- src/Providers/PDFServiceProvider.php
+++ src/Providers/PDFServiceProvider.php
@@ -36,34 +36,30 @@ class PDFServiceProvider extends ServiceProvider
     public function register()
     {
 
-        $this->app->bind('pdf', function ($cfg) {
-            if (!empty($cfg)) {
-                foreach ($cfg as $key => $value) {
-                    Config::set('pdf.' . $key, $value);
-                }
-            }
+        $this->app->bind('pdf', function ($app) {
+            $cfg=$app['config']['pdf'];
             $mpdf = new Mpdf(
-                Config::get('pdf')
+                $cfg
             );
             $permissions = [];
-            foreach (Config::get('pdf.protection.permissions') as $perm => $enable) {
+            foreach ($cfg['protection']['permissions'] as $perm => $enable) {
                 if ($enable) {
                     $permissions[] = $perm;
                 }
             }
             $mpdf->SetProtection(
                 $permissions,
-                Config::get('pdf.protection.user_password'),
-                Config::get('pdf.protection.owner_password'),
-                Config::get('pdf.protection.length')
+                $cfg['protection']['user_password'],
+                $cfg['protection']['owner_password'],
+                $cfg['protection']['length']
             );
-            $mpdf->SetTitle(Config::get('pdf.title'));
-            $mpdf->SetAuthor(Config::get('pdf.author'));
-            $mpdf->SetWatermarkText(Config::get('pdf.watermark'));
-            $mpdf->showWatermarkText = Config::get('pdf.showWatermark');
-            $mpdf->watermark_font = Config::get('pdf.watermarkFont');
-            $mpdf->watermarkTextAlpha = Config::get('pdf.watermarkTextAlpha');
-            $mpdf->SetDisplayMode(Config::get('pdf.displayMode'));
+            $mpdf->SetTitle($cfg['title']);
+            $mpdf->SetAuthor($cfg['author']);
+            $mpdf->SetWatermarkText($cfg['watermark']);
+            $mpdf->showWatermarkText = $cfg['showWatermark'];
+            $mpdf->watermark_font = $cfg['watermarkFont'];
+            $mpdf->watermarkTextAlpha = $cfg['watermarkTextAlpha'];
+            $mpdf->SetDisplayMode($cfg['displayMode']);
             return new PDFWrapper($mpdf);
         });
     }
